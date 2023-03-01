30be4234a432ef1e50092bbf10456dfd69cc10c0
DESKTOP-824QCP9\rozi <gpsyo911@gmail.com>
2023-03-02 02:08:45 +0900

    ㅎ

---
+++ src/com/greedy/sql/MemberController.java
@@ -0,0 +1,10 @@
+package com.greedy.sql;
+
+public class MemberController {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/com/greedy/sql/MemberMapper.java
@@ -0,0 +1,10 @@
+package com.greedy.sql;
+
+public class MemberMapper {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
